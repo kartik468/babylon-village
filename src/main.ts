351a480bb2379e7cc78b5c0811577f777813f822
kartikr <rkartik1992@gmail.com>
2023-08-15 23:55:05 +0530

    add material for house

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,35 @@ const createScene = () => {
     new BABYLON.Vector3(1, 1, 0)
   );
 
-  const box = BABYLON.MeshBuilder.CreateBox("box", {});
+  /**** Materials *****/
+  //color
+  const groundMat = new BABYLON.StandardMaterial("groundMat");
+  groundMat.diffuseColor = new BABYLON.Color3(0, 1, 0);
+
+  //texture
+  const roofMat = new BABYLON.StandardMaterial("roofMat");
+  roofMat.diffuseTexture = new BABYLON.Texture(
+    "https://assets.babylonjs.com/environments/roof.jpg"
+  );
+  const boxMat = new BABYLON.StandardMaterial("boxMat");
+  boxMat.diffuseTexture = new BABYLON.Texture(
+    "https://assets.babylonjs.com/environments/cubehouse.png"
+  );
+
+  //options parameter to set different images on each side
+  const faceUV = [];
+  faceUV[0] = new BABYLON.Vector4(0.5, 0.0, 0.75, 1.0); //rear face
+  faceUV[1] = new BABYLON.Vector4(0.0, 0.0, 0.25, 1.0); //front face
+  faceUV[2] = new BABYLON.Vector4(0.25, 0, 0.5, 1.0); //right side
+  faceUV[3] = new BABYLON.Vector4(0.75, 0, 1.0, 1.0); //left side
+  // top 4 and bottom 5 not seen so not set
+
+  /**** World Objects *****/
+  const box = BABYLON.MeshBuilder.CreateBox("box", {
+    faceUV: faceUV,
+    wrap: true,
+  });
+  box.material = boxMat;
   box.position.y = 0.5;
 
   const roof = BABYLON.MeshBuilder.CreateCylinder("roof", {
@@ -33,6 +61,7 @@ const createScene = () => {
     height: 1.2,
     tessellation: 3,
   });
+  roof.material = roofMat;
   roof.scaling.x = 0.75;
   roof.rotation.z = Math.PI / 2;
   roof.position.y = 1.22;
@@ -41,10 +70,10 @@ const createScene = () => {
     width: 10,
     height: 10,
   });
+  ground.material = groundMat;
 
   return scene;
 };
-
 const scene = await createScene();
 
 engine.runRenderLoop(() => {
